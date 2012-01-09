9834c92984b93a15bc0f802b22a4c3d3f3ba4a53
David <david.mazvovsky@gmail.com>
2012-01-10 00:15:26 +0200

    Added some early stuff

---
+++ SpeedyMailer.MailDrone.Tests/Maps/AutoMapperMaps.cs
@@ -0,0 +1,12 @@
+﻿using SpeedyMailer.Tests.Core;
+
+namespace SpeedyMailer.MailDrone.Tests.Maps
+{
+    public class AutoMapperMaps:IAutoMapperMaps
+    {
+        public void CreateMaps()
+        {
+        }
+    }
+}
+  
