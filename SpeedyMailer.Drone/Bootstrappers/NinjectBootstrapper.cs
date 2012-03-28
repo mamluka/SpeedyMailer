d10c4e3940959e4fea3107d0b4cc36dfba7d6cc2
davidm <davidm@delver.com>
2012-03-28 16:54:45 +0200

    Update some more

--- SpeedyMailer.Drone/Bootstrappers/NinjectBootstrapper.cs
+++ SpeedyMailer.Drone/Bootstrappers/NinjectBootstrapper.cs
@@ -1,13 +1,5 @@
 ﻿using Ninject;
-using Ninject.Modules;
-using RestSharp;
 using Ninject.Extensions.Conventions;
-using SpeedyMailer.Bridge.Communication;
-using SpeedyMailer.Core.Emails;
-using SpeedyMailer.Core.Helpers;
-using SpeedyMailer.Master.Web.UI.Communication;
-using SpeedyMailer.Master.Web.UI.Configurations;
-using SpeedyMailer.Master.Web.UI.Mail;
 
 namespace SpeedyMailer.Master.Web.UI.Bootstrappers
 {
@@ -18,38 +10,13 @@ namespace SpeedyMailer.Master.Web.UI.Bootstrappers
         static NinjectBootstrapper()
         {
            
-                Kernel = new StandardKernel();
-                Kernel.Scan(x=>
-                                {
-                                    x.FromAssembliesMatching("SpeedyMailer.*");
-                                    x.BindWith<DefaultBindingGenerator>();
-                                    
-                                });
-                //Kernel.Load<MailDroneStandardModule>();
-           
+            Kernel = new StandardKernel();
 
-        }
-    }
-
-    class MailDroneStandardModule : NinjectModule 
-    {
-        public override void Load()
-        {
-            Kernel.Bind<IDroneCommunicationService>().To<DroneCommunicationService>();
-            Kernel.Bind<IDroneMailOporations>().To<DroneMailOporations>();
-            Kernel.Bind<IMailSender>().To<MailSender>();
-            Kernel.Bind<IRestClient>().To<RestClient>();
-            Kernel.Bind<IDroneConfigurationManager>().To<DroneConfigurationManager>();
-            Kernel.Bind<IMailParser>().To<MailParser>();
-            Kernel.Bind<IEmailSourceWeaver>().To<EmailSourceWeaver>();
-            Kernel.Bind<IUrlCreator>().To<UrlCreator>();
-   
-
-
-
-
-
-
+        	Kernel.Bind(x =>
+        	            x.FromAssembliesMatching("SpeedyMailer.*")
+        	            	.SelectAllClasses()
+        	            	.BindAllInterfaces()
+        		);
         }
     }
 }
