f9ff7d4daa5240140f78bdfbfe93facbc59b1dc5
David <david.mazvovsky@gmail.com>
2012-08-24 22:50:52 +0300

    Stabilize solution with only Ninject for mono

--- SpeedyMailer.Drones.Tests.Integration/Commands/InitializeDroneSettingsCommandTests.cs
+++ SpeedyMailer.Drones.Tests.Integration/Commands/InitializeDroneSettingsCommandTests.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
+﻿using System.Net;
 using NUnit.Framework;
-using RestSharp;
 using SpeedyMailer.Core.Apis;
-using SpeedyMailer.Core.Container;
-using SpeedyMailer.Core.Settings;
 using SpeedyMailer.Drones.Commands;
 using SpeedyMailer.Drones.Settings;
 using SpeedyMailer.Tests.Core.Integration.Base;
