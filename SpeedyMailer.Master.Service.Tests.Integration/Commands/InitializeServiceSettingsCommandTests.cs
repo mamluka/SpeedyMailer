e98929a0599fffa428b8eee0327548cdbae602a4
davidmz <david.mazvovsky@gmail.com>
2012-08-31 01:02:34 -0700

    WIP

--- SpeedyMailer.Master.Service.Tests.Integration/Commands/InitializeServiceSettingsCommandTests.cs
+++ SpeedyMailer.Master.Service.Tests.Integration/Commands/InitializeServiceSettingsCommandTests.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using SpeedyMailer.Core.Commands;
+using SpeedyMailer.Tests.Core.Integration.Base;
 
 namespace SpeedyMailer.Master.Service.Tests.Integration.Commands
 {
     [TestFixture]
-    public class InitializeServiceSettingsCommandTests
+    public class InitializeServiceSettingsCommandTests:IntegrationTestBase
     {
-        
+        [Test]
+        public void Execute_WhenCalled_ShouldSetTheSettings()
+        {
+            ServiceActions.ExecuteCommand<InitializeServiceSettingsCommand>();
+        }
+    }
+
+    public class InitializeServiceSettingsCommand:Command
+    {
+        public string BaseUrl { get; set; }
+        public override void Execute()
+        {
+            
+        }
     }
 }
