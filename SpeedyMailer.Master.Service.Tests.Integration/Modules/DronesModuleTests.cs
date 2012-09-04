bf7d084055cb3e324801d7c3338aab07504b1138
davidmz <david.mazvovsky@gmail.com>
2012-09-03 22:48:28 -0700

    WIP

---
+++ SpeedyMailer.Master.Service.Tests.Integration/Modules/DronesModuleTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using SpeedyMailer.Core.Apis;
+using SpeedyMailer.Core.Domain.Drones;
+using SpeedyMailer.Core.Settings;
+using SpeedyMailer.Tests.Core.Integration.Base;
+
+namespace SpeedyMailer.Master.Service.Tests.Integration.Modules
+{
+    public class DronesModuleTests:IntegrationTestBase
+    {
+        [Test]
+        public void RegisterDrone_WhenCalledWithADroneIdentifier_ShouldRegisterTheDroneInTheStore()
+        {
+            ServiceActions.EditSettings<ServiceSettings>(x => { x.BaseUrl = DefaultBaseUrl; });
+
+            ServiceActions.Initialize();
+            ServiceActions.Start();
+
+            DroneActions.EditSettings<ApiCallsSettings>(x => { x.ApiBaseUri = DefaultBaseUrl; });
+            var api = DroneResolve<Api>();
+
+            api.Call<ServiceEndpoints.RegisterDrone>(x=> x.Identifier = "droneBaseUrl");
+
+            WaitForEntitiesToExist<Drone>(1);
+            var result = Query<Drone>().First();
+
+            result.Hostname.Should().Be("droneBaseUrl");
+        }
+    }
+}
---
+++ SpeedyMailer.Master.Service/Modules/DronesModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy;
+using Raven.Client;
+using SpeedyMailer.Core.Domain.Drones;
+
+namespace SpeedyMailer.Master.Service.Modules
+{
+    public class DronesModule : NancyModule
+    {
+        private readonly IDocumentStore _documentStore;
+
+        public DronesModule(IDocumentStore documentStore)
+            : base("/drones")
+        {
+            _documentStore = documentStore;
+
+            Post["/register"] = x =>
+                                    {
+                                        using (var session = _documentStore.OpenSession())
+                                        {
+                                            session.Store(new Drone
+                                                              {
+                                                                  Hostname = ""
+                                                              });
+                                        }
+                                        return Response.AsText("OK");
+                                    };
+        }
+    }
+}
