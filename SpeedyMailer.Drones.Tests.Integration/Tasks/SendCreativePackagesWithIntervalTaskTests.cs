8b77e684585cdc5285732f6159af5d0ff72312bc
davidmz <david.mazvovsky@gmail.com>
2012-10-07 18:11:10 +0200

    WIP

---
+++ SpeedyMailer.Drones.Tests.Integration/Tasks/SendCreativePackagesWithIntervalTaskTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SpeedyMailer.Tests.Core.Integration.Base;
+
+namespace SpeedyMailer.Drones.Tests.Integration.Tasks
+{
+	public class SendCreativePackagesWithIntervalTaskTests:IntegrationTestBase
+	{
+		[Test]
+		public void Execute_WhenTaskHasAnInterval_ShouldSendThePackagesAccordingToTheIntervalSpecified()
+		{
+			
+		}
+	}
+}
