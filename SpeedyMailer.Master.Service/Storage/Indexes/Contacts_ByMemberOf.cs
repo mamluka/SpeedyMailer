810a9fe47320cc3e76785bd365ca632475591b67
davidmz <david.mazvovsky@gmail.com>
2012-10-28 21:05:29 +0200

    Create an index template

---
+++ SpeedyMailer.Master.Service/Storage/Indexes/Contacts_ByMemberOf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Client.Indexes;
+using SpeedyMailer.Core.Domain.Contacts;
+
+namespace SpeedyMailer.Master.Service.Storage.Indexes
+{
+	public class Contacts_ByMemberOf:AbstractIndexCreationTask<Contact>
+	{
+		public Contacts_ByMemberOf()
+		{
+//			Map = contacts => contacts.Where(x=> x.MemberOf.Contains())
+		}
+	}
+}
