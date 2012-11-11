4f15a8f64f169629b709dca8f516dfbfde365924
davidmz <david.mazvovsky@gmail.com>
2012-11-11 22:52:21 +0200

    WIP

--- SpeedyMailer.Core/Tasks/SchedulerExtentions.cs
+++ SpeedyMailer.Core/Tasks/SchedulerExtentions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Quartz;
+using Quartz.Impl.Matchers;
 
 namespace SpeedyMailer.Core.Tasks
 {
@@ -14,6 +15,16 @@ namespace SpeedyMailer.Core.Tasks
 			{
 				target.Start();
 			}
+		}
+
+		public static void JobExistWithData<T>(this IScheduler target)
+		{
+			var groups = target.GetJobGroupNames();
+			var data = groups
+				.SelectMany(x => target.GetJobKeys(GroupMatcher<JobKey>.GroupEquals(x)))
+				.Where(x => x.Name.Contains(typeof(T).Name))
+				.Select(target.GetJobDetail)
+				.Any();
 		}
 	}
 }
