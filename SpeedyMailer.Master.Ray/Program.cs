a33269d9df297b70350146c0a12bdaeffd970ce6
davidmz <david.mazvovsky@gmail.com>
2012-11-21 06:34:53 +0200

    WIP

--- SpeedyMailer.Master.Ray/Program.cs
+++ SpeedyMailer.Master.Ray/Program.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
 
 namespace SpeedyMailer.Master.Ray
 {
 	class Program
 	{
+		public class RayCommandOptions : CommandLineOptionsBase
+		{
+			[Option("p", "process-csv", DefaultValue = @"http://localhost:2589", HelpText = "The base url of the service to register the drone with")]
+			public string ServiceBaseUrl { get; set; }
+		}
+
 		static void Main(string[] args)
 		{
+			var rayCommandOptions = new RayCommandOptions();
+			if (CommandLineParser.Default.ParseArguments(args, rayCommandOptions))
+			{
 
+			}
 		}
 	}
 }
