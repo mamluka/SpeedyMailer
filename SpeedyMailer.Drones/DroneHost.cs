0d55ade3404cece2afca393390956e30b2e83f89
davidmz <david.mazvovsky@gmail.com>
2012-12-07 18:11:48 +0200

    WIP

--- SpeedyMailer.Drones/DroneHost.cs
+++ SpeedyMailer.Drones/DroneHost.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 using CommandLine;
 using NLog;
 using Nancy.Bootstrapper;
@@ -16,68 +17,68 @@ using SpeedyMailer.Drones.Tasks;
 
 namespace SpeedyMailer.Drones
 {
-	public class DroneCommandOptions : CommandLineOptionsBase
-	{
-		[Option("s", "service-base-url", HelpText = "The base url of the service to register the drone with", Required = true)]
-		public string ServiceBaseUrl { get; set; }
+    public class DroneCommandOptions : CommandLineOptionsBase
+    {
+        [Option("s", "service-base-url", HelpText = "The base url of the service to register the drone with", Required = true)]
+        public string ServiceBaseUrl { get; set; }
 
-		[Option("b", "drone-base-url", HelpText = "The base url of the service to register the drone with", Required = false)]
-		public string BaseUrl { get; set; }
+        [Option("b", "drone-base-url", HelpText = "The base url of the service to register the drone with", Required = false)]
+        public string BaseUrl { get; set; }
 
-		[Option("N", "no-tasks", DefaultValue = false, HelpText = "The base url of the service to register the drone with", Required = false)]
-		public bool NoTasks { get; set; }
+        [Option("N", "no-tasks", DefaultValue = false, HelpText = "The base url of the service to register the drone with", Required = false)]
+        public bool NoTasks { get; set; }
 
-		[Option("P", "port", DefaultValue = 8080, HelpText = "The base url of the service to register the drone with", Required = false)]
-		public int RedirectedToListeningPort { get; set; }
-	}
-	public class DroneHost
-	{
-		public static void Main(string[] args)
-		{
-			var kernel = DroneContainerBootstrapper.Kernel;
+        [Option("P", "port", DefaultValue = 8080, HelpText = "The base url of the service to register the drone with", Required = false)]
+        public int RedirectedToListeningPort { get; set; }
+    }
+    public class DroneHost
+    {
+        public static void Main(string[] args)
+        {
+            var kernel = DroneContainerBootstrapper.Kernel;
 
-			var options = new DroneCommandOptions();
+            var options = new DroneCommandOptions();
 
-			if (CommandLineParser.Default.ParseArguments(args, options))
-			{
-				var initializeDroneSettingsCommand = kernel.Get<InitializeDroneSettingsCommand>();
-				initializeDroneSettingsCommand.RemoteConfigurationServiceBaseUrl = options.ServiceBaseUrl;
-				initializeDroneSettingsCommand.DroneBaseUrl = options.BaseUrl;
-				initializeDroneSettingsCommand.Execute();
+            if (CommandLineParser.Default.ParseArguments(args, options))
+            {
+                var initializeDroneSettingsCommand = kernel.Get<InitializeDroneSettingsCommand>();
+                initializeDroneSettingsCommand.RemoteConfigurationServiceBaseUrl = options.ServiceBaseUrl;
+                initializeDroneSettingsCommand.DroneBaseUrl = options.BaseUrl;
+                initializeDroneSettingsCommand.Execute();
 
-				var drone = kernel.Get<TopDrone>();
+                var drone = kernel.Get<TopDrone>();
 
-				drone.Initialize(options.RedirectedToListeningPort, options.NoTasks);
-				drone.Start();
+                drone.Initialize(options.RedirectedToListeningPort, options.NoTasks);
+                drone.Start();
 
-				Console.WriteLine("Starting drone...");
-			}
-		}
-	}
+                Console.WriteLine("Starting drone...");
+            }
+        }
+    }
 
-	public class TopDrone
-	{
-		private NancyHost _nancy;
-		private readonly INancyBootstrapper _nancyBootstrapper;
-		private readonly DroneSettings _droneSettings;
-		private readonly Framework _framework;
-		private readonly Logger _logger;
-		private readonly ApiCallsSettings _apiCallsSettings;
+    public class TopDrone
+    {
+        private NancyHost _nancy;
+        private readonly INancyBootstrapper _nancyBootstrapper;
+        private readonly DroneSettings _droneSettings;
+        private readonly Framework _framework;
+        private readonly Logger _logger;
+        private readonly ApiCallsSettings _apiCallsSettings;
 
-		public TopDrone(INancyBootstrapper nancyBootstrapper, Framework framework, DroneSettings droneSettings, ApiCallsSettings apiCallsSettings, Logger logger)
-		{
-			_apiCallsSettings = apiCallsSettings;
-			_logger = logger;
-			_framework = framework;
-			_droneSettings = droneSettings;
-			_nancyBootstrapper = nancyBootstrapper;
-		}
+        public TopDrone(INancyBootstrapper nancyBootstrapper, Framework framework, DroneSettings droneSettings, ApiCallsSettings apiCallsSettings, Logger logger)
+        {
+            _apiCallsSettings = apiCallsSettings;
+            _logger = logger;
+            _framework = framework;
+            _droneSettings = droneSettings;
+            _nancyBootstrapper = nancyBootstrapper;
+        }
 
-		public void Initialize(int redirectedToListeningPort = 8080, bool noTasks = false)
-		{
-			if (!noTasks)
-			{
-				var tasks = new List<ScheduledTask>
+        public void Initialize(int redirectedToListeningPort = 8080, bool noTasks = false)
+        {
+            if (!noTasks)
+            {
+                var tasks = new List<ScheduledTask>
 				{
 					new BroadcastDroneToServiceTask().DelayFor(TimeSpan.FromSeconds(30)),
 					new FetchCreativeFragmentsTask().DelayFor(TimeSpan.FromMinutes(1)),
@@ -88,25 +89,27 @@ namespace SpeedyMailer.Drones
 					new ResumePausedGroupsTask(),
 				};
 
-				_framework.StartTasks(tasks);
-			}
+                _framework.StartTasks(tasks);
+            }
 
-			_nancy = new NancyHost(new Uri(string.Format("{0}:{1}", _droneSettings.BaseUrl, redirectedToListeningPort)), _nancyBootstrapper);
-		}
+            var nancyUrl = Regex.Match(_droneSettings.BaseUrl, ":\\d{1}").Success ? _droneSettings.BaseUrl : string.Format("{0}:{1}", _droneSettings.BaseUrl, redirectedToListeningPort);
 
-		public void Start()
-		{
-			Trace.WriteLine("Drone started:" + _droneSettings.BaseUrl);
+            _nancy = new NancyHost(new Uri(nancyUrl), _nancyBootstrapper);
+        }
 
-			_nancy.Start();
+        public void Start()
+        {
+            Trace.WriteLine("Drone started:" + _droneSettings.BaseUrl);
 
-			_logger.Info("Drone started, master host is: {0}, drone host is: {1}", _apiCallsSettings.ApiBaseUri, new Uri(_droneSettings.BaseUrl).ToString());
-		}
+            _nancy.Start();
 
-		public void Stop()
-		{
-			_logger.Info("Drone stopped");
-			_nancy.Stop();
-		}
-	}
+            _logger.Info("Drone started, master host is: {0}, drone host is: {1}", _apiCallsSettings.ApiBaseUri, new Uri(_droneSettings.BaseUrl).ToString());
+        }
+
+        public void Stop()
+        {
+            _logger.Info("Drone stopped");
+            _nancy.Stop();
+        }
+    }
 }
---
+++ SpeedyMailer.Tests.Acceptance/Sending/SendingASingleEmailSpec.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpeedyMailer.Tests.Core.Integration.Base;
+
+namespace SpeedyMailer.Tests.Acceptance.Sending
+{
+    public class SendingASingleEmailSpec : IntegrationTestBase
+    {
+
+    }
+}
