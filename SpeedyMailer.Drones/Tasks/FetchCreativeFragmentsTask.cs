363a60e559d38ce5fa93a754dc0e4a83a3834186
davidmz <david.mazvovsky@gmail.com>
2012-12-12 00:16:39 +0200

    Added logging for groups delay

--- SpeedyMailer.Drones/Tasks/FetchCreativeFragmentsTask.cs
+++ SpeedyMailer.Drones/Tasks/FetchCreativeFragmentsTask.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using NLog;
 using Quartz;
 using SpeedyMailer.Core.Apis;
 using SpeedyMailer.Core.Domain;
@@ -14,128 +15,135 @@ using SpeedyMailer.Drones.Storage;
 
 namespace SpeedyMailer.Drones.Tasks
 {
-	public class FetchCreativeFragmentsTask : ScheduledTask
-	{
-		public override IJobDetail ConfigureJob()
-		{
-			return SimpleJob<Job>();
-		}
+    public class FetchCreativeFragmentsTask : ScheduledTask
+    {
+        public override IJobDetail ConfigureJob()
+        {
+            return SimpleJob<Job>();
+        }
 
-		public override ITrigger ConfigureTrigger()
-		{
-			return TriggerWithTimeCondition(x => x.WithIntervalInMinutes(1).RepeatForever());
-		}
+        public override ITrigger ConfigureTrigger()
+        {
+            return TriggerWithTimeCondition(x => x.WithIntervalInMinutes(1).RepeatForever());
+        }
 
-		[DisallowConcurrentExecution]
-		public class Job : IJob
-		{
-			private readonly Api _api;
-			private readonly Framework _framework;
-			private readonly CreativePackagesStore _creativePackagesStore;
-			private readonly OmniRecordManager _omniRecordManager;
-			private readonly MapToCreativePackageCommand _mapToCreativePackageCommand;
+        [DisallowConcurrentExecution]
+        public class Job : IJob
+        {
+            private readonly Api _api;
+            private readonly Framework _framework;
+            private readonly CreativePackagesStore _creativePackagesStore;
+            private readonly OmniRecordManager _omniRecordManager;
+            private readonly MapToCreativePackageCommand _mapToCreativePackageCommand;
+            private Logger _logger;
 
-			public Job(Framework framework,
-					   Api api,
-					   CreativePackagesStore creativePackagesStore,
-					   OmniRecordManager omniRecordManager,
-					   MapToCreativePackageCommand mapToCreativePackageCommand)
-			{
-				_mapToCreativePackageCommand = mapToCreativePackageCommand;
-				_omniRecordManager = omniRecordManager;
-				_framework = framework;
-				_creativePackagesStore = creativePackagesStore;
-				_api = api;
-			}
+            public Job(Framework framework,
+                       Api api,
+                       CreativePackagesStore creativePackagesStore,
+                       OmniRecordManager omniRecordManager,
+                       MapToCreativePackageCommand mapToCreativePackageCommand,
+                        Logger logger
+                )
+            {
+                _logger = logger;
+                _mapToCreativePackageCommand = mapToCreativePackageCommand;
+                _omniRecordManager = omniRecordManager;
+                _framework = framework;
+                _creativePackagesStore = creativePackagesStore;
+                _api = api;
+            }
 
-			public void Execute(IJobExecutionContext context)
-			{
-				var groupsSendingPolicies = _omniRecordManager.GetSingle<GroupsAndIndividualDomainsSendingPolicies>() ?? new GroupsAndIndividualDomainsSendingPolicies();
+            public void Execute(IJobExecutionContext context)
+            {
+                var groupsSendingPolicies = _omniRecordManager.GetSingle<GroupsAndIndividualDomainsSendingPolicies>() ?? new GroupsAndIndividualDomainsSendingPolicies();
 
-				if (_creativePackagesStore.AreThereAnyNonProcessedPackages())
-				{
-					var packages = _creativePackagesStore.GetAll();
+                if (_creativePackagesStore.AreThereAnyNonProcessedPackages())
+                {
+                    var packages = _creativePackagesStore.GetAll();
 
-					if (!context.Scheduler.IsJobsRunning<SendCreativePackagesWithIntervalTask>() && AreThereActiveGroups(packages, groupsSendingPolicies))
-					{
-						StartGroupSendingJobs(packages, groupsSendingPolicies);
+                    if (!context.Scheduler.IsJobsRunning<SendCreativePackagesWithIntervalTask>() && AreThereActiveGroups(packages, groupsSendingPolicies))
+                    {
+                        StartGroupSendingJobs(packages, groupsSendingPolicies);
 
-						return;
-					}
-				}
+                        return;
+                    }
+                }
 
-				var creativeFragment = _api
-					.Call<ServiceEndpoints.Creative.FetchFragment, CreativeFragment>();
+                var creativeFragment = _api
+                    .Call<ServiceEndpoints.Creative.FetchFragment, CreativeFragment>();
 
-				if (creativeFragment == null)
-					return;
+                if (creativeFragment == null)
+                    return;
 
-				SaveCurrentCreativeFragment(creativeFragment);
-				SaveCurrentCreativeToDealMap(creativeFragment);
+                SaveCurrentCreativeFragment(creativeFragment);
+                SaveCurrentCreativeToDealMap(creativeFragment);
 
-				var recipiens = creativeFragment.Recipients;
-				var creativePackages = recipiens.Select(x => ToCreativePackage(creativeFragment, x)).ToList();
+                var recipiens = creativeFragment.Recipients;
+                var creativePackages = recipiens.Select(x => ToCreativePackage(creativeFragment, x)).ToList();
 
-				_creativePackagesStore.BatchInsert(creativePackages);
+                _creativePackagesStore.BatchInsert(creativePackages);
 
-				StartGroupSendingJobs(creativePackages, groupsSendingPolicies);
-			}
+                StartGroupSendingJobs(creativePackages, groupsSendingPolicies);
+            }
 
-			private void SaveCurrentCreativeToDealMap(CreativeFragment creativeFragment)
-			{
-				_omniRecordManager.UpdateOrInsert(new CreativeToDealMap
-					                                  {
-						                                  Id = creativeFragment.CreativeId,
-														  DealUrl = creativeFragment.DealUrl
-					                                  });
-			}
+            private void StartGroupSendingJobs(IEnumerable<CreativePackage> recipiens, GroupsAndIndividualDomainsSendingPolicies sendingPolicies)
+            {
+                var groups = GetActiveGroups(recipiens, sendingPolicies);
 
-			private void SaveCurrentCreativeFragment(CreativeFragment creativeFragment)
-			{
-				var currentExecutingCreativeFragment = _omniRecordManager.GetSingle<CurrentExecutingCreativeFragment>() ?? new CurrentExecutingCreativeFragment();
-				currentExecutingCreativeFragment.CreativeFragment = creativeFragment;
+                foreach (var domainGroup in groups)
+                {
 
-				_omniRecordManager.UpdateOrInsert(currentExecutingCreativeFragment);
-			}
+                    _logger.Info("Start sending to group {0} with interval {1}", domainGroup.Group, domainGroup.Interval);
 
-			private CreativePackage ToCreativePackage(CreativeFragment creativeFragment, Recipient recipient)
-			{
-				_mapToCreativePackageCommand.CreativeFragment = creativeFragment;
-				_mapToCreativePackageCommand.Recipient = recipient;
-				return _mapToCreativePackageCommand.Execute();
-			}
+                    _framework.StartTasks(new SendCreativePackagesWithIntervalTask(x =>
+                        {
+                            x.Group = domainGroup.Group;
+                        },
+                                                                                   x => x.WithIntervalInSeconds(domainGroup.Interval).RepeatForever()
+                                              ));
+                }
+            }
 
-			private bool AreThereActiveGroups(IEnumerable<CreativePackage> packages, GroupsAndIndividualDomainsSendingPolicies groupsAndIndividualDomainsSendingPolicies)
-			{
-				return GetActiveGroups(packages, groupsAndIndividualDomainsSendingPolicies).Any();
-			}
+            private void SaveCurrentCreativeToDealMap(CreativeFragment creativeFragment)
+            {
+                _omniRecordManager.UpdateOrInsert(new CreativeToDealMap
+                                                      {
+                                                          Id = creativeFragment.CreativeId,
+                                                          DealUrl = creativeFragment.DealUrl
+                                                      });
+            }
 
-			private void StartGroupSendingJobs(IEnumerable<CreativePackage> recipiens, GroupsAndIndividualDomainsSendingPolicies andIndividualDomainsSendingPolicies)
-			{
-				var groups = GetActiveGroups(recipiens, andIndividualDomainsSendingPolicies);
+            private void SaveCurrentCreativeFragment(CreativeFragment creativeFragment)
+            {
+                var currentExecutingCreativeFragment = _omniRecordManager.GetSingle<CurrentExecutingCreativeFragment>() ?? new CurrentExecutingCreativeFragment();
+                currentExecutingCreativeFragment.CreativeFragment = creativeFragment;
 
-				foreach (var domainGroup in groups)
-				{
-					_framework.StartTasks(new SendCreativePackagesWithIntervalTask(x =>
-						                                                               {
-							                                                               x.Group = domainGroup.Group;
-						                                                               },
-					                                                               x => x.WithIntervalInSeconds(domainGroup.Interval).RepeatForever()
-						                      ));
-				}
-			}
+                _omniRecordManager.UpdateOrInsert(currentExecutingCreativeFragment);
+            }
 
-			private static IEnumerable<PackageInfo> GetActiveGroups(IEnumerable<CreativePackage> recipiens, GroupsAndIndividualDomainsSendingPolicies andIndividualDomainsSendingPolicies)
-			{
-				return recipiens
-					.GroupBy(x => x.Group)
-					.Select(x => new PackageInfo { Group = x.Key, Interval = x.First().Interval, Count = x.Count() })
-					.Where(x => !andIndividualDomainsSendingPolicies
-									 .GroupSendingPolicies
-									 .EmptyIfNull()
-									 .ContainsKey(x.Group))
-					.ToList();
-			}
-		}
-	}
+            private CreativePackage ToCreativePackage(CreativeFragment creativeFragment, Recipient recipient)
+            {
+                _mapToCreativePackageCommand.CreativeFragment = creativeFragment;
+                _mapToCreativePackageCommand.Recipient = recipient;
+                return _mapToCreativePackageCommand.Execute();
+            }
+
+            private bool AreThereActiveGroups(IEnumerable<CreativePackage> packages, GroupsAndIndividualDomainsSendingPolicies groupsAndIndividualDomainsSendingPolicies)
+            {
+                return GetActiveGroups(packages, groupsAndIndividualDomainsSendingPolicies).Any();
+            }
+
+            private static IEnumerable<PackageInfo> GetActiveGroups(IEnumerable<CreativePackage> recipiens, GroupsAndIndividualDomainsSendingPolicies andIndividualDomainsSendingPolicies)
+            {
+                return recipiens
+                    .GroupBy(x => x.Group)
+                    .Select(x => new PackageInfo { Group = x.Key, Interval = x.First().Interval, Count = x.Count() })
+                    .Where(x => !andIndividualDomainsSendingPolicies
+                                     .GroupSendingPolicies
+                                     .EmptyIfNull()
+                                     .ContainsKey(x.Group))
+                    .ToList();
+            }
+        }
+    }
 }
