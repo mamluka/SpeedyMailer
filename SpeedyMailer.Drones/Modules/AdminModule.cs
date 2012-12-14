86ab8ca863376e9301bcdc86a2bb9886804a0c0b
davidmz <david.mazvovsky@gmail.com>
2012-12-14 17:49:09 +0200

    List jobs

--- SpeedyMailer.Drones/Modules/AdminModule.cs
+++ SpeedyMailer.Drones/Modules/AdminModule.cs
@@ -14,73 +14,34 @@ using SpeedyMailer.Drones.Storage;
 
 namespace SpeedyMailer.Drones.Modules
 {
-    public class AdminModule : NancyModule
-    {
-        public AdminModule(IScheduler scheduler, LogsStore logsStore, ParseLogsCommand parseLogsCommand)
-            : base("/admin")
-        {
+	public class AdminModule : NancyModule
+	{
+		public AdminModule(IScheduler scheduler, LogsStore logsStore, ParseLogsCommand parseLogsCommand)
+			: base("/admin")
+		{
 
-            Get["/hello"] = x => Response.AsText("OK");
+			Get["/hello"] = x => Response.AsText("OK");
 
-            Get["/fire-task/{job}"] = x =>
-                                                    {
-                                                        scheduler.TriggerTaskByClassName((string)x.job);
-                                                        return Response.AsText("OK");
-                                                    };
+			Get["/fire-task/{job}"] = x =>
+													{
+														scheduler.TriggerTaskByClassName((string)x.job);
+														return Response.AsText("OK");
+													};
 
-            Get["/shutdown"] = x =>
-                                   {
-                                       var jobs = scheduler.GetCurrentJobs();
-                                       scheduler.DeleteJobs(jobs);
+			Get["/postfix-logs"] = x =>
+									   {
+										   var logs = logsStore.GetAllLogs();
+										   var lines = logs.Select(entry => string.Format("{0} {1}", entry.time.ToLongTimeString(), entry.msg)).ToList();
+										   return Response.AsText(string.Join(Environment.NewLine, lines));
+									   };
 
-                                       scheduler.Shutdown();
+			Get["/jobs"] = x =>
+				{
+					var tasks = scheduler.GetCurrentJobs();
+					var data = tasks.Select(jobKey => new { Data = scheduler.GetJobDetail(jobKey).JobDataMap, Job = jobKey.Name }).ToList();
 
-                                       while (!scheduler.IsShutdown)
-                                       {
-                                           Thread.Sleep(500);
-                                       }
-
-                                       Environment.Exit(0);
-
-                                       return Response.AsText("OK");
-                                   };
-
-            Get["/raw-postfix-logs"] = x => Response.AsJson(logsStore.GetAllLogs());
-
-            Get["/postfix-logs"] = x =>
-                                       {
-                                           var logs = logsStore.GetAllLogs();
-                                           var lines = logs.Select(entry => string.Format("{0} {1}", entry.time.ToLongTimeString(), entry.msg)).ToList();
-                                           return Response.AsText(string.Join(Environment.NewLine, lines));
-                                       };
-
-            Get["/write-headers"] = x =>
-                {
-                    var logs = logsStore.GetAllLogs();
-                    parseLogsCommand.Logs = logs;
-                    var parsedLogs = parseLogsCommand.Execute();
-
-
-                    var fakeLogs = parsedLogs.Select(
-                        mailEvent => new MailLogEntry
-	                        {
-		                        msg = mailEvent.MessageId + @": info: header Speedy-Creative-Id: creative/1 from localhost.localdomain[127.0.0.1]; from=<approval-department@xomixinc.com> to=<harveypchs@yahoo.com> proto=ESMTP helo=<mail>",
-		                        level = "INFO",
-		                        pid = "1235",
-		                        procid = "postfix",
-		                        sys = "mail",
-		                        syslog_fac = 2,
-		                        syslog_sever = 6,
-		                        syslog_tag = "postfix/smtpd[27723]:",
-		                        time = DateTime.UtcNow.AddDays(-10),
-		                        time_rcvd = DateTime.UtcNow.AddDays(-10)
-	                        }).ToList();
-
-                    logsStore.BatchInsert(fakeLogs);
-
-                    return Response.AsJson(fakeLogs);
-                };
-
-        }
-    }
+					return Response.AsJson(data);
+				};
+		}
+	}
 }
