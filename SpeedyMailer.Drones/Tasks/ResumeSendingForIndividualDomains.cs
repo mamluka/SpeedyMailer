59ce5675ab9ab60888c53a95881606d026389696
davidmz <david.mazvovsky@gmail.com>
2012-12-14 21:49:12 +0200

    adding more logging

--- SpeedyMailer.Drones/Tasks/ResumeSendingForIndividualDomains.cs
+++ SpeedyMailer.Drones/Tasks/ResumeSendingForIndividualDomains.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using NLog;
 using Quartz;
 using SpeedyMailer.Core.Domain.Mail;
 using SpeedyMailer.Core.Tasks;
@@ -7,51 +8,57 @@ using SpeedyMailer.Drones.Storage;
 
 namespace SpeedyMailer.Drones.Tasks
 {
-    public class ResumeSendingForIndividualDomains : ScheduledTask
-    {
-        public override IJobDetail ConfigureJob()
-        {
-            return SimpleJob<Job>();
-        }
+	public class ResumeSendingForIndividualDomains : ScheduledTask
+	{
+		public override IJobDetail ConfigureJob()
+		{
+			return SimpleJob<Job>();
+		}
 
-        public override ITrigger ConfigureTrigger()
-        {
-            return TriggerWithTimeCondition(x => x.WithIntervalInMinutes(30).RepeatForever());
-        }
+		public override ITrigger ConfigureTrigger()
+		{
+			return TriggerWithTimeCondition(x => x.WithIntervalInMinutes(30).RepeatForever());
+		}
 
-        public class Job : IJob
-        {
-            private readonly OmniRecordManager _omniRecordManager;
-            private readonly CreativePackagesStore _creativePackagesStore;
+		public class Job : IJob
+		{
+			private readonly OmniRecordManager _omniRecordManager;
+			private readonly CreativePackagesStore _creativePackagesStore;
+			private Logger _logger;
 
-            public Job(OmniRecordManager omniRecordManager,CreativePackagesStore creativePackagesStore)
-            {
-                _creativePackagesStore = creativePackagesStore;
-                _omniRecordManager = omniRecordManager;
-            }
+			public Job(OmniRecordManager omniRecordManager, CreativePackagesStore creativePackagesStore, Logger logger)
+			{
+				_logger = logger;
+				_creativePackagesStore = creativePackagesStore;
+				_omniRecordManager = omniRecordManager;
+			}
 
-            public void Execute(IJobExecutionContext context)
-            {
-                var sendingPolicies = _omniRecordManager.GetSingle<GroupsAndIndividualDomainsSendingPolicies>();
-                if (sendingPolicies == null)
-                    return;
+			public void Execute(IJobExecutionContext context)
+			{
+				var sendingPolicies = _omniRecordManager.GetSingle<GroupsAndIndividualDomainsSendingPolicies>();
+				if (sendingPolicies == null)
+					return;
 
-                var domainToResume = sendingPolicies.
-                    GroupSendingPolicies
-                    .Where(x => x.Value.ResumeAt < DateTime.UtcNow)
-                    .Select(x=> x.Key)
-                    .ToList();
+				var domainToResume = sendingPolicies.
+					GroupSendingPolicies
+					.Where(x => x.Value.ResumeAt < DateTime.UtcNow)
+					.Select(x => x.Key)
+					.ToList();
 
-                var matchedPackages = _creativePackagesStore.GetByDomains(domainToResume);
+				_logger.Info("Resuming domains for sending: {0}", string.Join(",", domainToResume));
 
-                matchedPackages
-                    .ToList()
-                    .ForEach(x =>
-                        {
-                            x.Processed = false;
-                            _creativePackagesStore.Save(x);
-                        });
-            }
-        }
-    }
+				var matchedPackages = _creativePackagesStore.GetByDomains(domainToResume);
+
+				_logger.Info("Resuming sending for packages: {0}", string.Join(",", matchedPackages.Select(x => x.To).ToList()));
+
+				matchedPackages
+					.ToList()
+					.ForEach(x =>
+						{
+							x.Processed = false;
+							_creativePackagesStore.Save(x);
+						});
+			}
+		}
+	}
 }
