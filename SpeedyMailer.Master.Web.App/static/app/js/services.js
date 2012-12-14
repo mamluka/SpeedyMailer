c81f32660e81c6197a8bf815a13966edbbe6fc25
davidmz <david.mazvovsky@gmail.com>
2012-12-14 13:46:30 +0200

    extract api base url

--- SpeedyMailer.Master.Web.App/static/app/js/services.js
+++ SpeedyMailer.Master.Web.App/static/app/js/services.js
@@ -2,9 +2,11 @@
 
 /* Services */
 
-
 // Demonstrate how to register services
 // In this case it is a simple value service.
+
+String.prototype.Apify = function () { return 'http://speedymailer.api' + this; };
+
 angular.module('SpeedyMailer.services', []).
   value('version', '0.1');
 
@@ -12,31 +14,31 @@ angular.module('SpeedyMailer.settings', []).
   value('apiSettings', { baseUrl: "http://speedymailer.api" });
 
 angular.module('SpeedyMailer.services', ['ngResource']).
-    factory('List', function($resource) {
-        return $resource('http://speedymailer.api/lists/list/:listId',
+    factory('List', function ($resource) {
+        return $resource('/lists/list/:listId'.Apify(),
             {
-                 listId: '@id'
+                listId: '@id'
             });
     }).
-    factory('Creative', function($resource) {
-        return $resource('http://speedymailer.api/creatives/:creativeId',
+    factory('Creative', function ($resource) {
+        return $resource('/creatives/:creativeId'.Apify(),
             {
                 creativeId: '@id'
             });
     }).
-    factory('Template', function($resource) {
-        return $resource('http://speedymailer.api/templates/:templateType/:templateId',
+    factory('Template', function ($resource) {
+        return $resource('/templates/:templateType/:templateId'.Apify(),
             {
                 templateId: '@id',
                 templateType: '@type'
             });
     }).
-    factory('TemplateType', function($resource) {
-        return $resource('http://speedymailer.api/templates/types/list');
+    factory('TemplateType', function ($resource) {
+        return $resource('/templates/types/list'.Apify());
     }).
     factory('Rule', function ($resource) {
-        return $resource('http://speedymailer.api/rules');
+        return $resource('/rules'.Apify());
     }).
     factory('DeliveryHeuristics', function ($resource) {
-        return $resource('http://speedymailer.api/heuristics/delivery');
+        return $resource('/heuristics/delivery'.Apify());
     });
