9ee2a28f5e63168734e91a9832153e7c4e470c25
davidmz <david.mazvovsky@gmail.com>
2012-12-14 18:32:11 +0200

    WIP

---
+++ SpeedyMailer.Drones.Tests.Integration/Events/RemoveSentPackagesTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using SpeedyMailer.Core.Domain.Creative;
+using SpeedyMailer.Core.Domain.Mail;
+using SpeedyMailer.Drones.Events;
+using SpeedyMailer.Tests.Core.Integration.Base;
+
+namespace SpeedyMailer.Drones.Tests.Integration.Events
+{
+	public class RemoveSentPackagesTests : IntegrationTestBase
+	{
+		public RemoveSentPackagesTests()
+			: base(x => x.UseMongo = true)
+		{ }
+
+		[Test]
+		public void Inspect_WhenAnPackageWasSent_ShouldRemoveItFromTheStore()
+		{
+			var package = new CreativePackage
+				{
+					To = "david@david.com",
+					Group = "$default$"
+				};
+
+			DroneActions.Store(package);
+
+			FireEvent<RemoveSentPackages, AggregatedMailSent>(x => x.MailEvents = new List<MailSent>
+				{
+					new MailSent
+						{
+							Recipient = "david@david.com"
+						}
+				});
+
+			var result = DroneActions.FindAll<CreativePackage>();
+
+			result.Should().BeEmpty();
+		}
+	}
+}
---
+++ SpeedyMailer.Drones/Events/RemoveSentPackages.cs
@@ -0,0 +1,13 @@
+﻿using SpeedyMailer.Core.Domain.Mail;
+using SpeedyMailer.Core.Evens;
+
+namespace SpeedyMailer.Drones.Events
+{
+	public class RemoveSentPackages:IHappendOn<AggregatedMailSent>
+	{
+		public void Inspect(AggregatedMailSent data)
+		{
+			
+		}
+	}
+}
