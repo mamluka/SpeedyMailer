95712cbdbe0f938f8c7dc172cc9fae61219b793b
davidmz <david.mazvovsky@gmail.com>
2013-01-05 19:48:39 +0200

    Added more indexes

--- SpeedyMailer.Master.Service/Storage/Indexes/Mapping_Indexes.cs
+++ SpeedyMailer.Master.Service/Storage/Indexes/Mapping_Indexes.cs
@@ -2,6 +2,7 @@
 using Raven.Client.Indexes;
 using SpeedyMailer.Core.Domain.Contacts;
 using SpeedyMailer.Core.Domain.Creative;
+using SpeedyMailer.Core.Tasks;
 
 namespace SpeedyMailer.Master.Service.Storage.Indexes
 {
@@ -18,6 +19,22 @@ namespace SpeedyMailer.Master.Service.Storage.Indexes
 		public Fragments_ByStatus()
 		{
 			Map = creativeFragments => creativeFragments.Select(x => new { x.Status });
+		}
+	}
+
+	public class Tasks_ByDateAndStatus : AbstractIndexCreationTask<PersistentTask>
+	{
+		public Tasks_ByDateAndStatus()
+		{
+			Map = persistentTasks => persistentTasks.Select(x => new { x.CreateDate, x.Status });
+		}
+	}
+
+	public class Templates_ByType : AbstractIndexCreationTask<Template>
+	{
+		public Templates_ByType()
+		{
+			Map = templates => templates.Select(x => new { x.Type });
 		}
 	}
 
