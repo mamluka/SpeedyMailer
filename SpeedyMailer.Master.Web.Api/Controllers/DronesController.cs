1699d489243be576540c13619e01c01215fb1889
davidmz <david.mazvovsky@gmail.com>
2013-01-08 15:48:28 +0200

    fixed the iOc

--- SpeedyMailer.Master.Web.Api/Controllers/DronesController.cs
+++ SpeedyMailer.Master.Web.Api/Controllers/DronesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using AttributeRouting.Web.Http;
 using SpeedyMailer.Core.Apis;
@@ -9,9 +10,9 @@ using SpeedyMailer.Core.Domain.Drones;
 
 namespace SpeedyMailer.Master.Web.Api.Controllers
 {
-    public class DronesController : Controller
+    public class DronesController : ApiController
     {
-	    private SpeedyMailer.Core.Apis.Api _api;
+	    private readonly SpeedyMailer.Core.Apis.Api _api;
 
 	    public DronesController(SpeedyMailer.Core.Apis.Api api)
 	    {
