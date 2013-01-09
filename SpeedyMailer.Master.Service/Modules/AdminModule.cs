416d401099904c5f573d4297c74f4958a9d568b3
davidmz <david.mazvovsky@gmail.com>
2013-01-09 20:21:25 +0200

    fix

--- SpeedyMailer.Master.Service/Modules/AdminModule.cs
+++ SpeedyMailer.Master.Service/Modules/AdminModule.cs
@@ -9,6 +9,7 @@ using Raven.Client.Linq;
 using SpeedyMailer.Core.Apis;
 using SpeedyMailer.Core.Container;
 using SpeedyMailer.Core.Domain.Contacts;
+using SpeedyMailer.Core.Domain.Creative;
 using SpeedyMailer.Core.Domain.Drones;
 using SpeedyMailer.Core.Settings;
 using SpeedyMailer.Core.Utilities;
@@ -75,6 +76,21 @@ namespace SpeedyMailer.Master.Service.Modules
 																				 .First(result => result.Group == "All")
 																				 .Exceptions.Distinct(new LambdaComparer<string>((m, n) => m.Substring(20) == n.Substring(20)))));
 					}
+				};
+
+			Get["/change-fragments"] = x =>
+				{
+					using (var session = documentStore.OpenSession())
+					{
+						var fragments = session.Query<CreativeFragment>().Where(q => q.CreativeId == "creatives/33").ToList();
+						fragments.ForEach(fragment =>
+							{
+								fragment.HtmlBody =
+									"We noticed two issues with your site.  It looks like it is not mobile ready, and only your competition has top placement in Google.  We can change that, and are willing to do it for free.  We offer free mobile website conversions to business who are not mobile ready, and one month of free Google top placement, so businesses can feel what it’s like to take their competitors prospects.<br><br> No gimmick no catch.  We do these things to show results.  Take a moment to see what your site would look like in proper mobile ready form.  Also take away your competitors phone calls.  It’s free.  Stop letting the other guy take all the business.<br><br> Visit <a href=\"http://centracorporation.com\">http://centracorporation.com</a><br> At the bottom of the page is a form.  Fill it out and we will do the rest.<br>We also have live chat support in case you have any questions.<br> Thank,<br> The Team @ Centra<br>";
+
+							});
+					}
+					return "OK";
 				};
 		}
 	}
