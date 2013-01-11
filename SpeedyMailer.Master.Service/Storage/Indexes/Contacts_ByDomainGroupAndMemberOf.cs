7b1111f5232e57f6b9b92426d46c6571d7769feb
davidmz <david.mazvovsky@gmail.com>
2013-01-11 13:11:32 +0200

    Added index to the domain group

---
+++ SpeedyMailer.Master.Service/Storage/Indexes/Contacts_ByDomainGroupAndMemberOf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Client.Indexes;
+using SpeedyMailer.Core.Domain.Contacts;
+
+namespace SpeedyMailer.Master.Service.Storage.Indexes
+{
+	public class Contacts_ByDomainGroupAndMemberOf : AbstractIndexCreationTask<Contact>
+	{
+		public Contacts_ByDomainGroupAndMemberOf()
+		{
+			Map = contact => contact.Select(x => new { x.DomainGroup, x.MemberOf });
+		}
+	}
+}
