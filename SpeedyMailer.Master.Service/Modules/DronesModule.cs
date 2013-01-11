e61ebe7d5d752c9ef3f9d74a45b14ab02208bcc3
davidmz <david.mazvovsky@gmail.com>
2013-01-11 12:34:34 +0200

    minor

--- SpeedyMailer.Master.Service/Modules/DronesModule.cs
+++ SpeedyMailer.Master.Service/Modules/DronesModule.cs
@@ -86,7 +86,7 @@ namespace SpeedyMailer.Master.Service.Modules
 				{
 					using (var session = documentStore.OpenSession())
 					{
-						var drones = session.Query<Drone>().Select(x => new SlimDrone { Id = x.Id, Domain = x.Domain }).ToList();
+						var drones = session.Query<Drone>().Select(drone => new SlimDrone { Id = drone.Id, Domain = drone.Domain }).ToList();
 						return Response.AsJson(drones);
 					}
 				};
