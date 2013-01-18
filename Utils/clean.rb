eacce582d119124116ffde595c78ff982de1bcd5
davidmz <david.mazvovsky@gmail.com>
2013-01-18 19:14:48 +0200

    wip

--- Utils/clean.rb
+++ Utils/clean.rb
@@ -6,12 +6,12 @@ data_file = ARGV.first
 
 puts "Extract domains"
 domain_file = data_file + ".domains.txt"
-puts `#{RAY_PATH} -p #{data_file} -o #{domain_file} -x`
+`#{RAY_PATH} -p #{data_file} -o #{domain_file} -x`
 
 puts "Run DNS clean"
 bad_domains = data_file + ".bad.domain.txt"
-puts `#{RAY_PATH} -p #{domain_file} -o #{bad_domains} -d`
+`#{RAY_PATH} -p #{domain_file} -o #{bad_domains} -d`
 
 puts "Output clean list"
 output_file = data_file + ".clean.txt"
-puts `#{RAY_PATH} -p #{domain_file} -o #{output_file} -b #{bad_domains}`
+`#{RAY_PATH} -p #{domain_file} -o #{output_file} -b #{bad_domains}`
