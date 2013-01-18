4d3bbff157f3c95daffe20115f9bba4e9b4413e3
davidmz <david.mazvovsky@gmail.com>
2013-01-18 19:12:09 +0200

    wip

--- Utils/clean.rb
+++ Utils/clean.rb
@@ -9,8 +9,9 @@ domain_file = data_file + ".domains.txt"
 puts `#{RAY_PATH} -p #{data_file} -o #{domain_file} -x`
 
 puts "Run DNS clean"
-bad_domains = data_file + "bad.domain.txt"
+bad_domains = data_file + ".bad.domain.txt"
 puts `#{RAY_PATH} -p #{domain_file} -o #{bad_domains} -d`
 
 puts "Output clean list"
-puts `#{RAY_PATH} -p #{domain_file} -o #{data_file + "clean.txt"} -b #{bad_domains}`
+output_file = data_file + ".clean.txt"
+puts `#{RAY_PATH} -p #{domain_file} -o #{output_file} -b #{bad_domains}`
