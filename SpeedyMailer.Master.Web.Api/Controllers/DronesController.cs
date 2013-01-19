2596d7cda4861777efcb3b545699b567396806d1
davidmz <david.mazvovsky@gmail.com>
2013-01-19 13:40:30 +0200

    yeah hack

--- SpeedyMailer.Master.Web.Api/Controllers/DronesController.cs
+++ SpeedyMailer.Master.Web.Api/Controllers/DronesController.cs
@@ -27,7 +27,7 @@ namespace SpeedyMailer.Master.Web.Api.Controllers
 		public IEnumerable<SlimDrone> GetDrones()
 		{
 			return _api.Call<ServiceEndpoints.Drones.Get, List<SlimDrone>>()
-				.Where(x=> x.LastUpdated > DateTime.UtcNow.AddMinutes(-10))
+				.Where(x=> x.LastUpdated.ToUniversalTime() > DateTime.UtcNow.AddMinutes(-10))
 				.ToList();
 		}
 
