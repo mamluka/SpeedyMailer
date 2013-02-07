b23868c3a24bb3acf8c0363819de94256064a546
davidmz <david.mazvovsky@gmail.com>
2013-02-07 23:00:47 +0200

    fix api call

--- SpeedyMailer.Core/Domain/Drones/SendingStatus.cs
+++ SpeedyMailer.Core/Domain/Drones/SendingStatus.cs
@@ -5,7 +5,7 @@ namespace SpeedyMailer.Core.Domain.Drones
 	public class SendingStatus
 	{
 		public long UnprocessedPackages { get; set; }
-		public IList<Group> Groups { get; set; }
+		public List<Group> Groups { get; set; }
 
 		public class Group
 		{
